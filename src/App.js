f6af4895d997bb72af24c8e198ae8a07bec9cc61
YashRawat21 <rawat17yash@gmail.com>
2023-09-20 13:20:05 +0530

    update commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
+import Body from './components/Body';
 function App() {
   return (
-    <div className="App">
-     <h1 className='text-blue-800 text-3xl'>Hello world</h1>
-    </div>
+    <Body />
   );
 }
 
