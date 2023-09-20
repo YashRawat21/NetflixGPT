f29250bd1db72820b1763ed7a666fdb8d85a2952
YashRawat21 <rawat17yash@gmail.com>
2023-09-20 21:10:50 +0530

    update commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { Login } from './Login'
-
+import Browse from './Browse'
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
 const Body = () => {
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: <Login />
+        },
+        {
+            path: "/browse",
+            element : <Browse />
+        }
+    ])
   return (
-    <Login />
+    <div>
+       <RouterProvider router = {appRouter}/>
+    </div>
   )
 }
 
