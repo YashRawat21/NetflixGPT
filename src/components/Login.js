2a53cf1a2ce9683c30709b9ec63dec25b1ee2b61
YashRawat21 <rawat17yash@gmail.com>
2023-09-20 21:50:14 +0530

    update commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,9 +5,16 @@ export const Login = () => {
   return (
     <div>
         <Header />
-        <div>
+        <div className='absolute'>
             <img src =  "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"/> 
         </div>
+        <form className='absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white bg-opacity-80'>
+            <h1 className='font-bold text-3xl py-4'>Sign In</h1>
+            <input type='text' placeholder='Email Address' className='p-4 my-2 w-full bg-gray-700' />
+            <input type='password' placeholder='Password' className='p-4 my-2 w-full bg-gray-700' />
+            <button className='p-4 my-6 bg-red-700 w-full rounded-lg'>Sign In</button>
+            <p className='py-4'>New to Netflix? Sign Up now</p>
+        </form>
     </div>
   )
 }
