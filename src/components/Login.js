352cabd4b0c6f9fdeaa0811d755119b97a9a7958
YashRawat21 <rawat17yash@gmail.com>
2023-09-28 01:25:35 +0530

    update commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,7 @@ import { auth } from '../utils/firebase';
 import {  updateProfile } from "firebase/auth";
 import { addUser } from '../utils/userSlice';
 import { useDispatch } from 'react-redux';
+import { BG_IMG } from '../utils/constants';
 
 export const Login = () => {
     const  [isSignIn , setIsSignIn] = useState(true)
@@ -74,7 +75,7 @@ export const Login = () => {
     <div>
         <Header />
         <div className='absolute'>
-            <img src =  "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"/> 
+            <img src =  {BG_IMG}/> 
         </div>
         <form onSubmit = {(e) => e.preventDefault()}className='absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white bg-opacity-80'>
             <h1 className='font-bold text-3xl py-4'>{isSignIn ? "Sign In" : "Sign Up" }</h1>
