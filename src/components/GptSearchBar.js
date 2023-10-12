16090d4cb4014e77b51c826c6b212c0e9f41f7b1
YashRawat21 <rawat17yash@gmail.com>
2023-10-12 09:54:51 +0530

    update commit

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const GptSearchBar = () => {
   return (
-    <div className='pt-[20%]'>
-        <form className=' bg-black w-1/2'>
-            <input className='p-4 m-4' type='text' placeholder='What would you like to watch today?'/>
-            <button className='px-4 py-2 bg-red-700 text-white rounded-lg'>Search</button>
+    <div className='pt-[10%] flex justify-center'>
+        <form className=' bg-black w-1/2  grid grid-cols-12'>
+            <input className='p-4 m-4 col-span-9' type='text' placeholder='What would you like to watch today?'/>
+            <button className='px-4 py-2 bg-red-700 text-white rounded-lg col-span-3 m-4'>Search</button>
         </form>
     </div>
   )
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -6,7 +6,7 @@ import VideoTitle from './VideoTitle'
 const MainContainer = () => {
     const movies = useSelector(store => store.movies?.nowPlayingMovies)
          if(movies ===null) return;
-    const mainMovie = movies[12];
+    const mainMovie = movies[10];
 
     const {original_title , overview,id} = mainMovie;
   return (
