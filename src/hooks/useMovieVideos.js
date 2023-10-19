6b0cac914b76449c9bd94484dba354476f6bcd13
YashRawat21 <rawat17yash@gmail.com>
2023-10-19 12:56:28 +0530

    update commit

--- src/hooks/useMovieVideos.js
+++ src/hooks/useMovieVideos.js
@@ -1,11 +1,11 @@
 import React, { useEffect} from 'react'
 import { API_OPTIONS } from '../utils/constants'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addTrailerVideo } from '../utils/movieSlice'
 
  export const useMovieVideos = (movieId) => {
 
-   
+   const trailerVideo = useSelector(store => store.movies.trailerVideo)
       const dispatch = useDispatch();
       const getMovieVideo = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/'+movieId+'/videos?language=en-US' , API_OPTIONS )
@@ -16,7 +16,7 @@ import { addTrailerVideo } from '../utils/movieSlice'
            dispatch(addTrailerVideo(trailer))  
       } 
       useEffect(()=> {
-        getMovieVideo()
+      if(!trailerVideo)  getMovieVideo()
       },[])
 }
 export default useMovieVideos;
