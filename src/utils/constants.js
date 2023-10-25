ab55b5afba5ce92e9fb19b669d85100bb3909357
YashRawat21 <rawat17yash@gmail.com>
2023-10-25 16:01:37 +0530

    update commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,4 +9,4 @@ export const API_OPTIONS = {
     },
   };
   export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
-  export const OPEN_AI_KEY = "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";
+  export const OPEN_AI_KEY = "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";
